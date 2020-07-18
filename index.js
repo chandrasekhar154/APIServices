74218f2d43d54b5c05b2c8d587cbbaaf84486f5e
chandrasekhar154 <chandrasekhar648@gmail.com>
2020-07-18 17:29:27 +0530

    index changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const startServer = async () => {
 	const port = process.env.SERVER_PORT || 4040;
 	try {
 		await promisify(app.listen).bind(app)(port);
-		console.log(`Listen on the port ${port}`);
+		// console.log(`Listen on the port ${port}`);
 	}
 	catch (error) {
 		console.log('Server started on port ' + port + ' with error ' + error);
