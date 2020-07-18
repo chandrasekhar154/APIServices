cb6e8d07a607dd88519b6a62aa54817c2bab375d
chandrasekhar154 <chandrasekhar648@gmail.com>
2020-07-18 17:40:57 +0530

    index change for heroku port

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const app = require('./app');
 const { promisify } = require('util');
 
-const startServer = async () => {
-	const port = process.env.SERVER_PORT || 4040;
-	try {
-		await promisify(app.listen).bind(app)(port);
-		// console.log(`Listen on the port ${port}`);
-	}
-	catch (error) {
-		console.log('Server started on port ' + port + ' with error ' + error);
-	}
-}
-startServer();
+// const startServer = async () => {
+// 	const port = process.env.SERVER_PORT || 4040;
+// 	try {
+// 		await promisify(app.listen).bind(app)(port);
+// 		// console.log(`Listen on the port ${port}`);
+// 	}
+// 	catch (error) {
+// 		console.log('Server started on port ' + port + ' with error ' + error);
+// 	}
+// }
+// startServer();
+
+const port = process.env.PORT || 8000;
+app.listen(process.env.PORT || port, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
